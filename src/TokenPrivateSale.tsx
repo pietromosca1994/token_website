5d3a21e102bdad8216601af61f5c7d4a98440439
Pietro Mosca <pietromosca1994@gmail.com>
2025-04-09 11:36:01 +0200

    fixed styles

--- src/TokenPrivateSale.tsx
+++ src/TokenPrivateSale.tsx
@@ -127,26 +127,6 @@ function PrivateSale() {
             </div>
 
             <button className="dimo-button buy-button">
-              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                />
-                <path
-                  d="M15 12H12V7"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M8 12H16"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                />
-              </svg>
               Buy $EAGLE Now
             </button>
           </section>
